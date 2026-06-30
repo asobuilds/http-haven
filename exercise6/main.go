0cb153384d55effa611bc368c2dd7868b2e8a02e
asobuilds <agenesunday143@gmail.com>
2026-06-30 11:34:16 +0100

    added new exercises

---
+++ exercise6/main.go
@@ -0,0 +1,20 @@
+package main
+
+import(
+	"fmt"
+	"net/http"
+)
+func dashboardHandler(w http.ResponseWriter, r *http.Request) {
+	key := r.Header.Get("X-API-Key")
+	if key != "secret123" {
+		http.Error(w, "invalid key", http.StatusUnauthorized)
+		return
+	}
+	fmt.Fprintf(w, "Welcome to your dashboard")
+		
+}
+func main() {
+	http.HandleFunc("/dashboard", dashboardHandler)
+	fmt.Println("server is 	running...")
+	http.ListenAndServe(":8080", nil)
+}
