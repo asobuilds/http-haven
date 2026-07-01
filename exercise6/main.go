840c934b440bdad98952f3171ecae67b44df5d89
asobuilds <agenesunday143@gmail.com>
2026-07-01 10:11:59 +0100

    learning how http works

--- exercise6/main.go
+++ exercise6/main.go
@@ -1,3 +1,35 @@
+// package main
+
+// import(
+// 	"fmt"
+// 	"net/http"
+// )
+// func dashboardHandler(w http.ResponseWriter, r *http.Request) {
+// 	key := r.Header.Get("X-API-Key")
+// 	if key != "secret123" {
+// 		http.Error(w, "invalid key", http.StatusUnauthorized)
+// 		return
+// 	}
+// 	fmt.Fprintf(w, "Welcome to your dashboard")
+		
+// }
+// func main() {
+// 	http.HandleFunc("/dashboard", dashboardHandler)
+// 	fmt.Println("server is 	running...")
+// 	http.ListenAndServe(":8080", nil)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
 package main
 
 import(
@@ -10,11 +42,10 @@ func dashboardHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "invalid key", http.StatusUnauthorized)
 		return
 	}
-	fmt.Fprintf(w, "Welcome to your dashboard")
-		
+	fmt.Fprint(w, "Welcome to your dashboard secret123")
 }
 func main() {
 	http.HandleFunc("/dashboard", dashboardHandler)
-	fmt.Println("server is 	running...")
+	fmt.Println("server is running...")
 	http.ListenAndServe(":8080", nil)
 }
