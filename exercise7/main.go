a8fc62f16814f3f7be9ec6729aa5c18f03631c4c
asobuilds <agenesunday143@gmail.com>
2026-07-01 10:29:19 +0100

    learning how http works, adding more exercises

---
+++ exercise7/main.go
@@ -0,0 +1,19 @@
+package main
+
+import(
+	"fmt"
+	"net/http"
+)
+func legacyHandler(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/v2", http.StatusMovedPermanently)
+}
+func v2Handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome to version 2")
+}
+
+func main() {
+	http.HandleFunc("/legacy", legacyHandler)
+	http.HandleFunc("/v2", v2Handler)
+	fmt.Println("server is running ...")
+	http.ListenAndServe(":8080", nil)
+}
